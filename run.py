6059001384811a9c6a1ffc228d508a0ee31ef3db
이제인 <janeyi@naver.com>
2020-08-11 17:18:01 +0900

    상단 네비게이션 바 생성 및 메뉴 구성

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 from app import app
 from convert import convert
 
 @app.route('/')
 def default():
-    return render_template('index.html', testStr = "hello world")
+    return render_template('index.html')
 
-@app.route('/test', methods=['POST', 'GET'])
-def test():
+@app.route('/ppt', methods=['POST', 'GET'])
+def ppt():
     if request.method == 'POST':
         inputHtmlStr = request.form.to_dict()['html']
         convert(inputHtmlStr)
-    return render_template('index2.html')
+    return render_template('ppt_index.html')
+
+@app.route('/image')
+def image():
+    return render_template('image_index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
