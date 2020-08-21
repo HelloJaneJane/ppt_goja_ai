1319176aba4b56000780e097b239bbe3f7b9e543
NamJiii <namghoon@gmail.com>
2020-08-21 17:29:49 +0900

    Update PyPPTModule.py

--- pptEngine/PyPPTModule.py
+++ pptEngine/PyPPTModule.py
@@ -37,8 +37,6 @@ def write(prs,file_name):
     prs.save(file_name)
 
 
-
-
 class TextData:
     def __init__(self, mainTitle, subTitle, midTitles, slideTitles):
         self._mainTitle = mainTitle
@@ -60,11 +58,12 @@ class PPTData:
         self._textData = textData
         # initial value
         self._topic = None
+        self._slideTypes = None
         self._basePrs = Presentation()
 
     # 전체 주제 getter
     @property
-    def topic(self):
+    def topic_(self):
         return self._topic
 
     # 전체 주제 setter
@@ -77,13 +76,54 @@ class PPTData:
 
     # # 베이스 테마 파일 getter
     # @property
-    # def basePrs(self):
-    #     return self._basePrs
+    def basePrs(self):
+        return self._basePrs
     
     # # 베이스 테마 파일 setter
     # @basePrs.setter
-    # def basePrs(self):
+    def basePrs(self):
     #     # topic에 어울리는 테마의 피피티를 고른다
-    #     basePrs = None
-    #     self._basePrs = basePrs
+        if self._topic=="ISW":
+            self._basePrs = Presentation("ISW.pptx")
+        else :
+            self.basePrs = Presentation("ISW.pptx")
+
+    def setSlideType(self):
+        self._slideTypes = [1,1,1,1,1,1]
+
+    def newSlide(self,slideType):
+        slide = self.basePrs.slides.add_slide(prs.slide_layouts[slideType])#ppt 객체, 슬라이드마스터 번호, 제목
+        slide.shapes.title.text = self._textData._mainTitle
+        return slide
+
+    #제목 슬라이드를 만드는 함수
+    def titleSlide(self):#NewSlide, 제목, 부제목
+        slide = newSlide(1)
+        slide.shapes.placeholders[1].text = self._textData._mainTitle
+        return slide
+
+    #슬라이드 객체의 특정 텍스트박스에 대한 설정
+    def setTextBox(self,slide,cnt,fontAdr):#슬라이드 객체, 텍스트박스 번호, 폰트주소
+        text_box = slide.shapes.placeholders[cnt].text_frame
+        text_box.fit_text(font_file=fontAdr)
+        return text_box
+
+    #텍스트박스 객체에 새로운 텍스트를 추가한다. (줄단위)
+    def newLine(textbox,text,font,size):#텍스트박스 객체, 내용, 폰트, 크기
+        line = textbox.add_paragraph()
+        line.font.name = font
+        line.font.size = Pt(size)
+        line.text = text
+
+    #ppt를 저장한다.
+    def write(prs,file_name):
+        prs.save(file_name)
+
+    def generate(self):
+        self.basePrs()
+        self.titleSlide()
+        for line in 
+        edit_slide = self.newSlide(self._slideType[0])
+        
+
 
